1131e2ee3a4ff73f30d10e82caabd8966a57e853
Mac <mac@SB-J.local>
2024-05-06 18:50:34 +0800

    feat: 增加mock

---
+++ packages/mock/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-red-400">yyds</div>
+</template>
+
+<script setup lang="ts">
+import { mockWorker } from './mocks/browser'
+
+mockWorker.start()
+console.log(123)
+</script>
+
---
+++ packages/mock/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    // to make tests faster
+    minify: false,
+  },
+})
